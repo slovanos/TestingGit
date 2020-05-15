bf118065399b8ae6c44502fbdd8ff3be6024267c
apolos <apolos@gmail.com>
2020-05-15 15:55:06 +0200

    feature of the public branch

--- hello.py
+++ hello.py
@@ -6,6 +6,4 @@ print('Agrego un print al principio más otra linea')
 name = myname.get_name()
 print(f'hello {name}')
 
-print('Shine on you crazy Diamond, Pink Floyd')
-
-print('Agrego un print al final en la rama')
+print('THIS IS PART OF THE BRANCH THAT GOES PUBLIC')
